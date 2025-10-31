de52ecfdd427ded28184e2d56ed01d8acc3378de
72566633-stack <72566633@continental.edu.pe>
2025-10-30 23:46:56 -0500

    estructuraPaciente
    estructuraPaciente

--- CodigoEstructura.cpp
+++ CodigoEstructura.cpp
@@ -3,3 +3,15 @@ estructura de datos
 */
 #include <iostream>
 using namespace std;
+
+struct Paciente {
+    string nombre;
+    string dni;
+    int edad;
+    string motivo;
+    int prioridad; // 1=Urgente,2=Normal,3=Baja
+    string estado; // "En espera", "En atención", "Atendido"
+    bool tieneReceta;
+    string rutaReceta;
+    Paciente() : edad(0), prioridad(2), estado("En espera"), tieneReceta(false) {}
+};
